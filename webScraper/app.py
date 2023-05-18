c520f816cd5f1e52675f933d375e81a9f38e8053
MaeFlaherty <Spencerflaherty53@gmail.com>
2023-05-18 17:43:09 -0400

    moved search.html and searchs.css to the templates and static folder respectively, added functionality to the recipeSearch template

--- webScraper/app.py
+++ webScraper/app.py
@@ -1,35 +1,48 @@
 import web
 import webScraper
 from webScraper import Recipe
+import recipeSearch 
 
 urls = (
-    '/(.*)', 'index'
+	'/(.*)', 'search'
 )
+
 app = web.application(urls, globals())
 
 class index:
-    def __init__(self):
-        self.render = web.template.render("templates/")
+	def __init__(self):
+		self.render = web.template.render("templates/")
 
-    def GET(self, link):
-        if not link:
-            link = 'Error, Link Invalid'
-            return link
+	def GET(self, link):
+		if not link:
+			link = 'Error, Link Invalid'
+			return link
 
-        else:
-            try:
-                data = webScraper.get_ld_json(link)
-            except:
-                print("ld+json not found")
-                link = "Couldn't generate recipe, try again"
-                return link
+		else:
+			try:
+				data = webScraper.get_ld_json(link)
+			except:
+				print("ld+json not found")
+				link = "Couldn't generate recipe, try again"
+				return link
 
-            recipe = Recipe(data)
-            
-            return self.render.index(recipe.name, recipe.ingredients, recipe.instructions, recipe.image)
-            
+			recipe = Recipe(data)
+			
+			return self.render.index(recipe.name, recipe.ingredients, recipe.instructions, recipe.image)
+			
+class search:
+	def __init__(self):
+		self.render = web.template.render("templates/")
 
-            
+	def GET(self, term = str):
+		try:
+			searchList = recipeSearch.pySearch(term)
+		except:
+			searchList = ["name", "name", "name", "name", "name", "name"]
+
+		return self.render.search(term, searchList)
+
+
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
